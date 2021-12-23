e8e4e3042e9005a9c8ec913ab88dbd6a9d1ac9d1
kbogorodsky <konstantin.bogorodsky@gmail.com>
2021-12-23 11:56:33 +0300

    erlang maps / revise theory

--- main.erl
+++ main.erl
@@ -1,3 +1,11 @@
 -module(main).
 
--record(user, {id: integer(), name: string(), age: integer(), country: string(), city: string()}).
+-record(user, { id:: integer(),
+                name:: string(),
+                age:: integer(),
+                country:: string(),
+                city:: string() }).
+
+-export([hello_world/0]).
+
+hello_world() -> io:fwrite("Hello World!").
